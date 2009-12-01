7a958f93ee05857e082cf08fffb612b6361aca98
Weston Ruter <westonruter@gmail.com>
2009-12-01 01:00:00 +0000

    Initial checking of dev files

---
+++ wp-manage.pl
@@ -0,0 +1,541 @@
+#!/usr/bin/perl -w
+# WordPress Manager Script
+# by Weston Ruter <weston@shepherd-interactive.com>
+# Copyright 2009, Shepherd Interaction <http://shepherdinteractive.com/>. GPL License.
+# $Id$
+
+use warnings;
+use strict;
+use open ':utf8';
+use Getopt::Std;
+
+
+our $VERSION = '0.3dev';
+my $isDebug = 1;
+
+# All of the possible subcommands
+my %subcommands = (
+	init      => {
+		description => "Start a new WordPress install using configuration file."
+	},
+	dumpdata  => {
+		description => "Dump the database from the specified environment creating versions for each of the other environments.",
+		arguments   => [
+			{
+				name => 'environment',
+				optional => 1
+			}
+		]
+	},
+	pushdata  => {
+		description => "Take the latest dump and push it to the supplied or default environment.",
+		arguments   => [
+			{
+				name => 'environment',
+				optional => 1
+			}
+		]
+	},
+	help      => {
+		description => "Displays this screen."
+	},
+	update    => {
+		description => "Updates the WP version and plugins defined in config file and then does svn up."
+	},
+);
+
+# Get the invoked subcommand
+my $subcommand = lc shift @ARGV;
+die "First argument must be valid subcommand; run wp-manage.pl help" if($subcommand && not exists $subcommands{$subcommand});
+$subcommand ||= 'help';
+
+# All of the valid options
+my %options = (
+	'c' => {
+		default     => './config.json',
+		description => "The configuration file (defaults to ./config.json)"
+	},
+	'v' => {
+		description => "Verbose mode"
+	},
+	'f' => {
+		description => "Force the potentially destructive action to occur",
+		subcommands => [qw( pushdata )]
+	}
+);
+
+# Get the allowed option arguments for this subcommand
+my $optstring = '';
+foreach(keys %options){
+	$optstring .= $_;
+	$optstring .= ':' if exists $options{$_}->{default};
+}
+
+# Get the arguments
+my %args;
+getopts($optstring, \%args);
+
+
+# Supply default argument option values
+foreach my $switch (keys %options){
+	if((not exists $args{$switch}) && (exists $options{$switch}->{default})){
+		$args{$switch} = $options{$switch}->{default};
+	}
+}
+
+# Detect illegal arguments
+foreach my $switch (keys %args){
+	if((exists $options{$switch}->{subcommands}) && !grep /$subcommand/, @{$options{$switch}->{subcommands}}){
+		die "Illegal option '$switch' for subcommand '$subcommand'";
+	}
+}
+
+
+# If no subcommand given, display help
+if($subcommand eq 'help'){
+	print "\n";
+	print " \n\n";
+	print "\n";
+
+	print <<HELP;
+WordPress Manager Script, version $VERSION
+  by Weston Ruter <weston\@shepherd-interactive.com>
+Usage: perl wp-manage.pl <subcommand> [options] [args]
+  perl wp-manage.pl init
+  perl wp-manage.pl init -c ./config.json
+  perl wp-manage.pl update
+  perl wp-manage.pl dumpdata development
+  perl wp-manage.pl pushdata staging
+  perl wp-manage.pl pushdata -f production
+
+Suggestion: checkout wp-manage.pl to a svn:externals directory and then in the
+directory above store config.js and various shell scripts to invoke various
+commands. A typical project root should look like this:
+  /wp-manage/wp-manage.pl
+  /config.js
+  /dumpdev-pushstaging.sh
+  /db/development.sql
+  /db/staging.sql
+  /db/production.sql
+  /public/index.php
+  /public/wp-config.php
+  ...
+  /public/wp-content/...
+
+HELP
+
+	
+	print "Available subcommands:\n";
+	foreach my $key (sort keys(%subcommands)){
+		printf("   % -10s%s\n", $key, $subcommands{$key}->{description});
+	}
+	exit;
+}
+
+
+# Load configuration file
+my $configFile = $args{'c'}; #"./config.json"
+use JSON;
+-f $configFile or die "Config file does not exist $configFile";
+open CONFIG, $configFile or die "Unable to read from $configFile";
+my $config = decode_json(join '', <CONFIG>);
+close CONFIG;
+
+#use Data::Dumper;
+#print Dumper($config);
+#my $environment = shift @ARGV || $config->{default_environment};
+#print "\n##$environment##";
+#exit;
+
+
+die "Error: The current directory must be an SVN working copy.\n" if not -e '.svn';
+
+my $svn_verbose_switch   .= exists $args{v} ? ' --verbose ' : '';
+my $mysql_verbose_switch .= exists $args{v} ? ' --verbose ' : '';
+
+use File::Basename;
+sub in_array #http://www.go4expert.com/forums/showthread.php?t=8978
+{
+	my $search_for = shift;
+	my %items = map {$_ => 1} @_; # create a hash out of the array values
+	return exists $items{$search_for};
+}
+
+
+# Setup new installation
+if($subcommand eq 'setup' || $subcommand eq 'install' || $subcommand eq 'init'){
+	
+	# Create directories and add them to repo
+	temp_system("svn $svn_verbose_switch mkdir " . ($config->{'db_dump_dir'} || 'db'));
+	my $public_dir = ($config->{'public_dir'} || 'public');
+	temp_system("svn $svn_verbose_switch mkdir $public_dir");
+	temp_system("svn $svn_verbose_switch mkdir $public_dir/wp-content");
+	temp_system("svn $svn_verbose_switch mkdir $public_dir/wp-content/uploads");
+	chmod(0777,      "$public_dir/wp-content/uploads");
+	temp_system("svn $svn_verbose_switch mkdir $public_dir/wp-content/cache");
+	chmod(0777,      "$public_dir/wp-content/cache");
+	temp_system("svn $svn_verbose_switch mkdir $public_dir/wp-content/plugins");
+	chmod(0777,      "$public_dir/wp-content/plugins");
+	
+	my($wp_repo_url, $wp_repo_rev) = split(/\?r=/, $config->{wp_repo});
+	$wp_repo_rev ||= "HEAD";
+	$wp_repo_url .= '/' if $wp_repo_url !~ m{/$}; #trailingslashit
+	my $wp_repo_rev_arg = '';
+	if($wp_repo_rev ne 'HEAD'){
+		$wp_repo_rev_arg = "-r $wp_repo_rev";
+	}
+	
+	# Grab the 
+	temp_system("svn export $svn_verbose_switch --force --non-recursive $wp_repo_rev_arg $wp_repo_url $public_dir");
+	
+	# Ignore files
+	my @ignore = ();
+	push @ignore, "wp-config-sample.php";
+	push @ignore, "license.txt";
+	push @ignore, "readme.html";
+	open EXT, ">~temp.txt";
+	print EXT join("\n", @ignore);
+	close EXT;
+	temp_system("svn propset svn:ignore -F ~temp.txt $public_dir");
+	
+	# Add files
+	chdir($public_dir);
+	foreach(<*>){
+		if(-f && !in_array($_, @ignore)){
+			temp_system("svn add $_");
+		}
+	}
+	chdir('..');
+	#temp_system("svn add $public_dir");
+	
+	# Load the admin and includes directories
+	use File::Path;
+	
+	my @externals = ();
+	rmtree("$public_dir/wp-admin");
+	push @externals, "wp-admin      $wp_repo_rev_arg   ${wp_repo_url}wp-admin";
+	rmtree("$public_dir/wp-includes");
+	push @externals, "wp-includes   $wp_repo_rev_arg   ${wp_repo_url}wp-includes";
+	open EXT, ">~temp.txt";
+	print EXT join("\n", @externals);
+	close EXT;
+	temp_system("svn propset svn:externals -F ~temp.txt $public_dir");
+	
+	# Add plugins
+	@externals = ();
+	open EXT, ">~temp.txt";
+	#foreach my $url (@{$config->{plugin_repos}}){
+	while(my($pluginDir, $urlWithRepo) = each(%{$config->{plugin_repos}})){
+		my($plugin_repo_url, $plugin_rev) = split(/\?r=/, $urlWithRepo);
+		$plugin_rev ||= "HEAD";
+		$plugin_repo_url .= '/' if $plugin_repo_url !~ m{/$}; #trailingslashit
+		my $plugin_rev_arg = '';
+		if($plugin_rev ne 'HEAD'){
+			$plugin_rev_arg = " -r$plugin_rev ";
+		}
+		push @externals, "$pluginDir  $plugin_rev_arg  $plugin_repo_url";
+	}
+	print EXT join("\n", @externals);
+	close EXT;
+	temp_system("svn propset svn:externals -F ~temp.txt $public_dir/wp-content/plugins");
+	
+	
+	# Setup the wp-config.php file
+	use LWP::Simple;
+	my $authenticationUniqueKeys = get("http://api.wordpress.org/secret-key/1.1/");
+	my $charset = $config->{db_charset} || 'utf8';
+	my $table_prefix = exists $config->{db_table_prefix} ? $config->{db_table_prefix} : 'wp_';
+	
+	my $mySqlConstants = '';
+	my $c;
+	foreach my $env (keys %{$config->{'environments'}}){
+		next if $env eq 'production';
+		$c = $config->{'environments'}->{$env};
+		
+		$mySqlConstants .= $mySqlConstants ? "else if(" : "if(";
+		$mySqlConstants .= '$_SERVER["HTTP_HOST"] == "' . $c->{http_host} . '"';
+		$mySqlConstants .= "){\n";
+		$mySqlConstants .= "\tdefine('DB_NAME', '" . $c->{'db_name'} . "');\n";
+		$mySqlConstants .= "\tdefine('DB_USER', '" . $c->{'db_user'} . "');\n";
+		$mySqlConstants .= "\tdefine('DB_PASSWORD', '" . ($c->{'db_password'}) . "');\n";
+		$mySqlConstants .= "\tdefine('DB_HOST', '" . ($c->{'db_host'} || '127.0.0.1') . "');\n";
+		$mySqlConstants .= "\tdefine('WP_DEBUG', " . ($c->{'debug'}) . ");\n" if $c->{'debug'};
+		$mySqlConstants .= "}\n";
+	}
+	
+	if(exists $config->{'environments'}->{'production'}){
+		$c = $config->{'environments'}->{'production'};
+		$mySqlConstants .= "else {\n";
+		$mySqlConstants .= "\tdefine('DB_NAME', '" . $c->{'db_name'} . "');\n";
+		$mySqlConstants .= "\tdefine('DB_USER', '" . $c->{'db_user'} . "');\n";
+		$mySqlConstants .= "\tdefine('DB_PASSWORD', '" . ($c->{'db_password'}) . "');\n";
+		$mySqlConstants .= "\tdefine('DB_HOST', '" . ($c->{'db_host'} || '127.0.0.1') . "');\n";
+		$mySqlConstants .= "\tdefine('WP_DEBUG', " . ($c->{'debug'}) . ");\n" if $c->{'debug'};
+		$mySqlConstants .= "}\n";
+	}
+	
+	open WPCONFIG, ">$public_dir/wp-config.php";
+	print WPCONFIG <<WPCONFIGFILE;
+<?php
+/** 
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information by
+ * visiting {\@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * \@package WordPress
+ */
+
+// ** MySQL settings ** //
+$mySqlConstants
+
+define('DB_CHARSET', '$charset');
+define('DB_COLLATE', '');
+
+/**#\@+
+ * Authentication Unique Keys.
+ */
+$authenticationUniqueKeys
+/**#\@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+\$table_prefix  = '$table_prefix';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress.  A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de.mo to wp-content/languages and set WPLANG to 'de' to enable German
+ * language support.
+ */
+define ('WPLANG', '$config->{lang}');
+
+/* That's all, stop editing! Happy blogging. */
+
+/** WordPress absolute path to the Wordpress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+WPCONFIGFILE
+	close WPCONFIG;
+	
+	temp_system("svn add $public_dir/wp-config.php");
+	
+	# Create databases development only (staging and production must be setup manually)
+	if(exists $config->{environments}->{$config->{default_environment}}){
+		$c = $config->{environments}->{$config->{default_environment}};
+		open SQL, ">~temp.txt";
+		print SQL "CREATE DATABASE `$c->{db_name}`";
+		print SQL " DEFAULT CHARACTER SET $config->{db_charset}" if $config->{db_charset};
+		print SQL ";";
+		close SQL;
+		temp_system("mysql $mysql_verbose_switch -u $c->{db_user} --password=$c->{db_password} < ~temp.txt");
+	}
+	
+	#foreach my $env (keys %{$config->{'environments'}}){
+	#	my $c = $config->{'environments'}->{$env};
+	#	#if($c->{db_user})
+	#	
+	#	temp_system("mysql $mysql_verbose_switch -u $c->{db_user} --password=$c->{db_password}");
+	#}
+	
+	# Add a theme
+	temp_system("svn mkdir $svn_verbose_switch $public_dir/wp-content/themes");
+	temp_system("svn mkdir $svn_verbose_switch $public_dir/wp-content/themes/" . $config->{theme_slug});
+	open CSS, ">$public_dir/wp-content/themes/" . $config->{theme_slug} . "/style.css";
+	print CSS "/*
+Theme Name: $config->{site_name}
+Author: $config->{theme_author}
+*/";
+	close CSS;
+	temp_system("svn add $public_dir/wp-content/themes/" . $config->{theme_slug} . "/style.css");
+	open PHP, ">$public_dir/wp-content/themes/" . $config->{theme_slug} . "/index.php";
+	print PHP "Theme is empty";
+	close PHP;
+	temp_system("svn add $public_dir/wp-content/themes/" . $config->{theme_slug} . "/index.php");
+	temp_system("svn up $svn_verbose_switch");
+	
+	#Week Starts On: Sunday
+	# 
+	#Ensure that uploads_dir is set to wp-content/uploads
+	#Ensure that uploads are not uploaded into categories
+	#Turn on Permalinks
+	#Set admin email and site name
+	#Set theme
+	
+	unlink('~temp.txt');
+	exit;
+}
+
+
+# Updates the WP version to the version located at config.wp_repo and does svn up
+if($subcommand eq 'update'){
+	my $public_dir = ($config->{'public_dir'} || 'public');
+	
+	# Update the WP Install
+	my($wp_repo_url, $wp_repo_rev) = split(/\?r=/, $config->{wp_repo});
+	$wp_repo_rev ||= "HEAD";
+	$wp_repo_url .= '/' if $wp_repo_url !~ m{/$}; #trailingslashit
+	my $wp_repo_rev_arg = '';
+	if($wp_repo_rev ne 'HEAD'){
+		$wp_repo_rev_arg = "-r $wp_repo_rev";
+	}
+	temp_system("svn export $svn_verbose_switch --force --non-recursive $wp_repo_rev_arg $wp_repo_url $public_dir");
+	
+	my $externals = `svn propget svn:externals $public_dir`;
+	$externals =~ s{(-r\d+\s*)?http://(core\.svn\.wordpress\.org/tags|svn\.automattic\.com/wordpress/tags)/(\d+(\.\d+)*)/}
+	                 {$wp_repo_rev_arg$wp_repo_url}g;
+	$externals =~ s{\s+$}{\n}s; #remove trailing slashes
+	open TEMP, ">~propset.txt";
+	print TEMP $externals;
+	close TEMP;
+	temp_system("svn propset svn:externals -F ~propset.txt $public_dir ");
+	unlink('~propset.txt');
+	
+	
+	# Update the plugins
+	my @externals = ();
+	open EXT, ">~temp.txt";
+	#foreach my $url (@{$config->{plugin_repos}}){
+	while(my($pluginDir, $urlWithRepo) = each(%{$config->{plugin_repos}})){
+		my($plugin_repo_url, $plugin_rev) = split(/\?r=/, $urlWithRepo);
+		$plugin_rev ||= "HEAD";
+		$plugin_repo_url .= '/' if $plugin_repo_url !~ m{/$}; #trailingslashit
+		my $plugin_rev_arg = '';
+		if($plugin_rev ne 'HEAD'){
+			$plugin_rev_arg = " -r$plugin_rev ";
+		}
+		push @externals, "$pluginDir  $plugin_rev_arg  $plugin_repo_url";
+	}
+	print EXT join("\n", @externals);
+	close EXT;
+	temp_system("svn propset svn:externals -F ~temp.txt $public_dir/wp-content/plugins");
+	temp_system("svn up $svn_verbose_switch");
+	
+	#print "Now do svn up and svn commit to have your changes enacted.\n";
+	unlink('~temp.txt');
+	exit;
+}
+
+
+
+# Take the data from an environment and dump it out so it can be imported into each other environment
+if($subcommand eq 'dumpdata' || $subcommand eq 'datadump'){
+	#Get the destination environment
+	my $environment = shift @ARGV || $config->{default_environment};
+	die "Unrecognized environment '$environment'\n" if not exists $config->{'environments'}->{$environment};
+	my $c = $config->{'environments'}->{$environment};
+	
+	#The servers other than primary are the destinations
+	my @environments = keys(%{$config->{environments}});
+	my @destinations = grep !/^$environment$/i, @environments;
+	
+	#Dump the source database
+	print " - Getting dump from $environment...";
+	temp_system('mysqldump ' .
+			join(' ', (
+				$mysql_verbose_switch,
+				'--host "' . ($c->{db_host} || $c->{http_host}) . '"',
+				'--user "' . $c->{db_user} . '"',
+				'--password="' . $c->{db_password} . '"',
+				'--extended-insert=FALSE',
+				'--skip-comments',
+				'--no-create-db',
+				'--databases "' . $c->{db_name} . '"'
+			)) . ' > ~temp.sql');
+	print "done.\n";
+	
+	my $db_dump_dir = ($config->{'db_dump_dir'} || 'db');
+	
+	open SRC_IN, "~temp.sql";
+	open SRC_OUT, ">$db_dump_dir/$environment.sql";
+	
+	#Open write-filehandles for the destinations
+	my %dest_fh = ();
+	foreach my $dest (@destinations){
+		open $dest_fh{$dest}, ">$db_dump_dir/$dest.sql";
+	}
+	
+	while(<SRC_IN>){
+		#s{^-- Dump completed on .+}{};
+		#s{^-- Server version.+}{};
+		#s{^-- Host:.+}{};
+		
+		#Problem with WP2.5
+		#s{^INSERT INTO `wp_options`.+?,dashboard_widget_options'.+}{}s;
+		
+		print SRC_OUT;
+	
+		my $original = $_;
+		foreach my $dest (@destinations){
+			s{(?<=://)\Q$config->{environments}->{$environment}->{http_host}\E(?!\.)}
+			 {$config->{environments}->{$dest}->{http_host}}g;
+			 
+			#s{USE `\Q$config->{environments}->{$environment}->{db_name}\E`;}
+			# {USE `$config->{environments}->{$dest}->{db_name}`;}g;
+			 
+			#s{(CREATE DATABASE.+?)`\Q$sources{$source}->{db_name}\E`}
+			 #{$1`$sources{$dest}->{db_name}`};
+			
+			select $dest_fh{$dest};
+			print;
+			
+			$_ = $original;
+		}
+	}
+	select STDOUT;
+	
+	close SRC_IN;
+	close SRC_OUT;
+	print " - Finished $environment\n";
+	
+	foreach my $dest (keys %dest_fh){
+		print " - Finished $dest\n";
+		close $dest_fh{$dest};
+	}
+	
+	unlink('~temp.sql');
+	exit;
+}
+
+# Take the data from an environment and dump it out so it can be imported into each other environment
+if($subcommand eq 'pushdata' || $subcommand eq 'datapush'){
+	
+	my $db_dump_dir = ($config->{'db_dump_dir'} || 'db');
+	
+	my $isForce = exists $args{f};
+	
+	#Get the destination environment
+	my $environment = shift @ARGV || $config->{default_environment};
+	die "Unrecognized environment '$environment'\n" if not exists $config->{'environments'}->{$environment};
+	my $c = $config->{'environments'}->{$environment};
+	
+	die "Error: In order to push to production, you must supply the -f parameter\n" if($c->{force_required} && !$isForce);
+	die "Error: $db_dump_dir/$environment.sql does not exist. Please run dumpdata\n" if(not -f "$db_dump_dir/$environment.sql");
+	
+	my $db_host = $c->{db_host} || $c->{http_host};
+	temp_system("mysql $mysql_verbose_switch -h $db_host -u $c->{db_user} --password=\"$c->{db_password}\" < $db_dump_dir/$environment.sql");
+	exit;
+}
+
+
+
+sub temp_system {
+	my $cmd = shift;
+	print $cmd . "\n";
+}
