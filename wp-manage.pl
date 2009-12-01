3917ae315f5b264643a945d4bb6c85f480f918e2
Weston Ruter <westonruter@gmail.com>
2009-12-01 21:41:10 +0000

    Adding license block

--- wp-manage.pl
+++ wp-manage.pl
@@ -1,8 +1,23 @@
 #!/usr/bin/perl -w
 # WordPress Manager Script
 # by Weston Ruter <weston@shepherd-interactive.com>
-# Copyright 2009, Shepherd Interaction <http://shepherdinteractive.com/>. GPL License.
+# Copyright 2009, Shepherd Interaction <http://shepherdinteractive.com/>
 # $Id$
+# 
+# License: GPL 3.0 <http://www.gnu.org/licenses/gpl.html>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use warnings;
 use strict;
